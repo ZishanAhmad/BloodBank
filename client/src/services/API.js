6518fb7984616d90f8d26533e06df697ee294ba7
ZishanAhmad <79711278+ZishanAhmad@users.noreply.github.com>
2023-07-13 14:38:30 +0530

    commit

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,5 +1,5 @@
 import axios from "axios"; 
-const API = axios.create({ baseURL: 'http://localhost:5000/api/v1' });
+const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
