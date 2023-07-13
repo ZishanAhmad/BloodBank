1d8e3691511924983240a218a21a5a2ec137938e
ZishanAhmad <79711278+ZishanAhmad@users.noreply.github.com>
2023-07-13 12:19:37 +0530

    first commit

---
+++ middlewares/authMiddelware.js
@@ -0,0 +1,26 @@
+const JWT = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    // console.log(req.headers["authorization"]);
+    const token = req.headers["authorization"].split(" ")[1];
+    JWT.verify(token, 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.bQTnz6AuMJvmXXQsVPrxeQNvzDkimo7VNXxHeSBfClLufmCVZRUuyTwJF311JHuh', (err, decode) => {
+      if (err) {
+        return res.status(401).send({
+          success: false,
+          message: "Auth Failed",
+        });
+      } else {
+        req.body.userId = decode.userId;
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).send({
+      success: false,
+      error,
+      message: "Auth Failedd",
+    });
+  }
+};
